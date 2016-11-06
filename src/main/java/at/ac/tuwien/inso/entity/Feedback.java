86590c28261b2b41033f5742742bf2c9989966d1
Enri Miho <e0929003@student.tuwien.ac.at>
2016-11-07 00:49:58 +0100

    add relationships for Feedback and Tag

--- src/main/java/at/ac/tuwien/inso/entity/Feedback.java
+++ src/main/java/at/ac/tuwien/inso/entity/Feedback.java
@@ -1,8 +1,10 @@
 package at.ac.tuwien.inso.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
 
 @Entity
 public class Feedback {
@@ -11,4 +13,76 @@ public class Feedback {
     @GeneratedValue
     private Long id;
 
+    @ManyToOne
+    private Student student;
+
+    @ManyToOne
+    private Course course;
+
+    @ManyToMany
+    private List<Tag> tags = new ArrayList<>();
+
+    protected Feedback() {}
+
+    public Feedback(Student student, Course course) {
+        this.student = student;
+        this.course = course;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void addTags(Tag... tags) {
+        this.tags.addAll(asList(tags));
+    }
+
+    public void removeTags(Tag... tags) {
+        this.tags.removeAll(asList(tags));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Feedback feedback = (Feedback) o;
+
+        if (!id.equals(feedback.id)) return false;
+        if (!student.equals(feedback.student)) return false;
+        if (!course.equals(feedback.course)) return false;
+        return tags.equals(feedback.tags);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + student.hashCode();
+        result = 31 * result + course.hashCode();
+        result = 31 * result + tags.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Feedback{" +
+                "id=" + id +
+                ", student=" + student +
+                ", course=" + course +
+                ", tags=" + tags +
+                '}';
+    }
 }
--- src/main/java/at/ac/tuwien/inso/entity/Tag.java
+++ src/main/java/at/ac/tuwien/inso/entity/Tag.java
@@ -1,5 +1,6 @@
 package at.ac.tuwien.inso.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -11,4 +12,47 @@ public class Tag {
     @GeneratedValue
     private Long id;
 
+    @Column(unique = true)
+    private String name;
+
+    protected Tag() {}
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Tag tag = (Tag) o;
+
+        if (!id.equals(tag.id)) return false;
+        return name.equals(tag.name);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Tag{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
