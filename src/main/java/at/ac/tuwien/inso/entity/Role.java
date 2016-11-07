90facedc4526cf5c567e524387caaab575f30793
Enri Miho <e0929003@student.tuwien.ac.at>
2016-11-07 12:39:40 +0100

    authority should be unique; add equals and toString

--- src/main/java/at/ac/tuwien/inso/entity/Role.java
+++ src/main/java/at/ac/tuwien/inso/entity/Role.java
@@ -11,6 +11,7 @@ public class Role implements GrantedAuthority {
     @GeneratedValue
     private Long id;
 
+    @Column(unique = true)
     private String authority;
 
     protected Role() {
@@ -23,5 +24,32 @@ public class Role implements GrantedAuthority {
     @Override
     public String getAuthority() {
         return authority;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+
+        if (!id.equals(role.id)) return false;
+        return authority.equals(role.authority);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + authority.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", authority='" + authority + '\'' +
+                '}';
     }
 }
