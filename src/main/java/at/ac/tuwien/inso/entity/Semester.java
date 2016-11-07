c2496156d5bec74a701988269aace236c5499604
Enri Miho <e0929003@student.tuwien.ac.at>
2016-11-07 01:32:48 +0100

    add some details about Semester.

--- src/main/java/at/ac/tuwien/inso/entity/Semester.java
+++ src/main/java/at/ac/tuwien/inso/entity/Semester.java
@@ -1,5 +1,6 @@
 package at.ac.tuwien.inso.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -11,4 +12,41 @@ public class Semester {
     @GeneratedValue
     private Long id;
 
+    @Column
+    private String label;
+
+    public Semester(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Semester semester = (Semester) o;
+
+        if (!id.equals(semester.id)) return false;
+        return label.equals(semester.label);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + label.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Semester{" +
+                "id=" + id +
+                ", label='" + label + '\'' +
+                '}';
+    }
 }
