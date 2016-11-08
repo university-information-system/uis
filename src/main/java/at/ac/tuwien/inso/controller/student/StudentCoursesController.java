7796b70f999a004d8ec89048099353b0519d87eb
Damjan Gjurovski <e1129609@student.tuwien.ac.at>
2016-11-08 22:48:51 +0100

    Add search string, filter for name

--- src/main/java/at/ac/tuwien/inso/controller/student/StudentCoursesController.java
+++ src/main/java/at/ac/tuwien/inso/controller/student/StudentCoursesController.java
@@ -6,6 +6,7 @@ import at.ac.tuwien.inso.service.CoursesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -17,12 +18,23 @@ public class StudentCoursesController {
     private CoursesService coursesService;
 
     @ModelAttribute("allCourses")
-    private List<Course> getAllCourses() {
-        return coursesService.findForCurrentSemester();
+    private List<Course> getAllCourses(@RequestParam(value = "search", required = false) String search) {
+        final String searchString = getSearchString(search);
+        return coursesService.findCourseForCurrentSemesterWithName(searchString);
+    }
+
+    @ModelAttribute("searchString")
+    private String getSearchString(@RequestParam(value = "search", required = false) String search) {
+        if (search != null && !search.isEmpty()) {
+            return search;
+        }
+
+        return "";
     }
 
     @GetMapping
     public String courses() {
         return "/student/courses";
     }
+
 }
