d73fb52bae16a75806012f69da76149f1bf9b51d
Damjan Gjurovski <e1129609@student.tuwien.ac.at>
2016-11-08 19:28:20 +0100

    Add view for listing current semester courses

--- src/main/java/at/ac/tuwien/inso/controller/student/StudentCoursesController.java
+++ src/main/java/at/ac/tuwien/inso/controller/student/StudentCoursesController.java
@@ -1,11 +1,25 @@
 package at.ac.tuwien.inso.controller.student;
 
+import at.ac.tuwien.inso.entity.Course;
+import at.ac.tuwien.inso.service.CoursesService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/student/courses")
 public class StudentCoursesController {
+
+    @Autowired
+    private CoursesService coursesService;
+
+    @ModelAttribute("allCourses")
+    private List<Course> getAllCourses() {
+        return coursesService.findForCurrentSemester();
+    }
 
     @GetMapping
     public String courses() {
