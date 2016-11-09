ba54a87fe4869943c6f9857f8ec355fcd49c5eb9
Enri Miho <e0929003@student.tuwien.ac.at>
2016-11-09 19:27:38 +0100

    show subjects in admin view. also added edit and delete buttons.

--- src/main/java/at/ac/tuwien/inso/controller/admin/AdminSubjectsController.java
+++ src/main/java/at/ac/tuwien/inso/controller/admin/AdminSubjectsController.java
@@ -1,5 +1,8 @@
 package at.ac.tuwien.inso.controller.admin;
 
+import at.ac.tuwien.inso.entity.Subject;
+import at.ac.tuwien.inso.service.SubjectService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,8 +10,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/admin/subjects")
 public class AdminSubjectsController {
 
+    @Autowired
+    private SubjectService subjectService;
+
     @GetMapping
     public String subjects() {
         return "admin/subjects";
+    }
+
+    @ModelAttribute("subjects")
+    private Iterable<Subject> getOwnSubjects() {
+        return subjectService.getAllSubjects();
     }
 }
