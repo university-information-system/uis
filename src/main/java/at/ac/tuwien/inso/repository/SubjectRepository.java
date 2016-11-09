7a923821c9e789b8a1377a71cc50c24e8d8c8ec8
Enri Miho <e0929003@student.tuwien.ac.at>
2016-11-09 15:09:33 +0100

    find subjects for lecturer

--- src/main/java/at/ac/tuwien/inso/repository/SubjectRepository.java
+++ src/main/java/at/ac/tuwien/inso/repository/SubjectRepository.java
@@ -2,6 +2,10 @@ package at.ac.tuwien.inso.repository;
 
 import at.ac.tuwien.inso.entity.*;
 import org.springframework.data.repository.*;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SubjectRepository extends CrudRepository<Subject, Long> {
+
+    Iterable<Subject> findByLecturers_Id(Long id);
 }
