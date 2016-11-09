36e820a6195a16e21a7df467232d174280d4f6a2
Enri Miho <e0929003@student.tuwien.ac.at>
2016-11-09 15:08:20 +0100

    find lecturer by account id (unfortunately as a native sql query)

--- src/main/java/at/ac/tuwien/inso/repository/LecturerRepository.java
+++ src/main/java/at/ac/tuwien/inso/repository/LecturerRepository.java
@@ -1,7 +1,11 @@
 package at.ac.tuwien.inso.repository;
 
 import at.ac.tuwien.inso.entity.*;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.*;
 
 public interface LecturerRepository extends CrudRepository<Lecturer, Long> {
+
+    @Query("select l from Lecturer l where ACCOUNT_ID = ?1")
+    Lecturer findLecturerByAccountId(Long id);
 }
