4cf0bcdc509ae3101b42f9abf83f5b0fb9b39b4f
Enri Miho <e0929003@student.tuwien.ac.at>
2016-11-09 15:12:12 +0100

    implement view and controller for owned subjects.

--- src/main/java/at/ac/tuwien/inso/controller/lecturer/LecturerSubjectsController.java
+++ src/main/java/at/ac/tuwien/inso/controller/lecturer/LecturerSubjectsController.java
@@ -1,5 +1,8 @@
 package at.ac.tuwien.inso.controller.lecturer;
 
+import at.ac.tuwien.inso.entity.Subject;
+import at.ac.tuwien.inso.service.LecturerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,8 +10,17 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/lecturer/subjects")
 public class LecturerSubjectsController {
 
+    @Autowired
+    private LecturerService lecturerService;
+
     @GetMapping
     public String subjects() {
         return "lecturer/subjects";
     }
+
+    @ModelAttribute("ownedSubjects")
+    private Iterable<Subject> getOwnSubjects() {
+        return lecturerService.getOwnSubjects();
+    }
+
 }
