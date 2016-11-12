776a2ddc480872a346a00cf50ab55983ac870e6f
Michael Pazourek <e1225721@student.tuwien.ac.at>
2016-11-12 01:41:37 +0100

    added add button for subjects. added add-page. added post method in
    controller.

--- src/main/java/at/ac/tuwien/inso/controller/admin/AdminSubjectsController.java
+++ src/main/java/at/ac/tuwien/inso/controller/admin/AdminSubjectsController.java
@@ -32,4 +32,21 @@ public class AdminSubjectsController {
         model.addAttribute("requiredSubjects", subject.getRequiredSubjects());
         return "admin/subject-details";
     }
+    
+    @GetMapping("/add")
+    private String addSubject(Model model) {
+        /*Subject subject = subjectService.getSubjectById(id);
+        model.addAttribute("subject", subject);
+        model.addAttribute("lecturers", subject.getLecturers());
+        model.addAttribute("requiredSubjects", subject.getRequiredSubjects());*/
+    	model.addAttribute("subject", subjectService.getNewSubject());
+        return "admin/subject-add";
+    }
+    
+    @PostMapping("/add")
+    private String addSubject(@ModelAttribute Subject subject) {
+    	System.out.println(subject.getName());//immer null.. TODO, check warum
+    	System.out.println("ects"+subject.getEcts());
+    	return "admin/subject-details";
+    }
 }
