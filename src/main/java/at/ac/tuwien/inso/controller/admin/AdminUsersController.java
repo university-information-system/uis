e7da6b116c6858f4682c8c1e9e851191c6ed428a
Valentin - Mihai Neacsu <neacsustoi_92@yahoo.com>
2016-11-13 10:34:52 +0100

    fix lecturer subjects padding

--- src/main/java/at/ac/tuwien/inso/controller/admin/AdminUsersController.java
+++ src/main/java/at/ac/tuwien/inso/controller/admin/AdminUsersController.java
@@ -1,15 +1,5 @@
 package at.ac.tuwien.inso.controller.admin;
 
-import at.ac.tuwien.inso.entity.*;
-import at.ac.tuwien.inso.service.LecturerService;
-import at.ac.tuwien.inso.service.StudentService;
-import at.ac.tuwien.inso.service.UisUserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import at.ac.tuwien.inso.controller.admin.forms.*;
 import at.ac.tuwien.inso.entity.*;
 import at.ac.tuwien.inso.service.*;
