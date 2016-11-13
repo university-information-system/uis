9d84f8e5c1a62e629df67d3ecf68bb2598cb2668
Damjan Gjurovski <e1129609@student.tuwien.ac.at>
2016-11-13 23:39:21 +0100

    Add view for courses for lecturer

--- src/main/java/at/ac/tuwien/inso/controller/lecturer/LecturerCoursesController.java
+++ src/main/java/at/ac/tuwien/inso/controller/lecturer/LecturerCoursesController.java
@@ -1,11 +1,31 @@
 package at.ac.tuwien.inso.controller.lecturer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import at.ac.tuwien.inso.entity.Course;
+import at.ac.tuwien.inso.entity.Lecturer;
+import at.ac.tuwien.inso.service.CourseService;
+import at.ac.tuwien.inso.service.LecturerService;
 
 @Controller
 @RequestMapping("/lecturer/courses")
 public class LecturerCoursesController {
+
+    @Autowired
+    private CourseService courseService;
+    @Autowired
+    private LecturerService lecturerService;
+
+
+    @ModelAttribute("allCourses")
+    private List<Course> getAllCourses() {
+        Lecturer lecturer = lecturerService.getLoggedInLecturer();
+        return courseService.findCoursesForCurrentSemesterForLecturer(lecturer);
+    }
 
     @GetMapping
     public String courses() {
