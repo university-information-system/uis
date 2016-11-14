988717e3e0d31158c091d06c9c737468c3b8f99e
Damjan Gjurovski <e1129609@student.tuwien.ac.at>
2016-11-14 13:52:15 +0100

    Add js method for showing checkboxes with materialize

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,6 +3,14 @@ const main = {
     initialize: function() {
         $(document).ready(function(){
             $('.button-collapse').sideNav();
+            moveLabelsForCheckboxes();
         })
     }
 };
+
+function moveLabelsForCheckboxes() {  //the label must be moved because materialize.css expects the input and label to be one after the other and spring mvc adds an extra hidden input field
+    $('.checkboxLabel').each(function () {
+        if(this.previousElementSibling)
+            this.parentNode.insertBefore(this, this.previousElementSibling);
+    })
+}
