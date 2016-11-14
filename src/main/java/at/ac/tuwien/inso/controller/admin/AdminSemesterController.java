e9a4d73194452f4bea9db9362d162cebcf320d66
Martin Stücklschwaiger <martin.st@wolke7.net>
2016-11-14 10:19:29 +0100

    Create new semesters

--- src/main/java/at/ac/tuwien/inso/controller/admin/AdminSemesterController.java
+++ src/main/java/at/ac/tuwien/inso/controller/admin/AdminSemesterController.java
@@ -1,12 +1,18 @@
 package at.ac.tuwien.inso.controller.admin;
 
+import at.ac.tuwien.inso.controller.admin.forms.CreateSemesterForm;
+import at.ac.tuwien.inso.controller.admin.forms.CreateUserForm;
 import at.ac.tuwien.inso.entity.Semester;
 import at.ac.tuwien.inso.service.SemesterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.*;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/admin/semester")
@@ -28,5 +34,27 @@ public class AdminSemesterController {
     @GetMapping
     public String semester() {
         return "admin/semester";
+    }
+
+    @GetMapping("/create")
+    public String createSemesterView(CreateSemesterForm createSemesterForm) {
+        return "admin/create-semester";
+    }
+
+    @PostMapping("/create")
+    public String createSemester(@Valid CreateSemesterForm form,
+                                 BindingResult bindingResult,
+                                 RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            return "admin/create-semester";
+        }
+
+        Semester submittedSemester = form.toSemester();
+
+        System.out.println(submittedSemester);
+
+        semesterService.create(submittedSemester);
+
+        return "redirect:/admin/semester";
     }
 }
---
+++ src/main/java/at/ac/tuwien/inso/controller/admin/forms/CreateSemesterForm.java
@@ -0,0 +1,52 @@
+package at.ac.tuwien.inso.controller.admin.forms;
+
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.NotNull;
+
+import at.ac.tuwien.inso.entity.Semester;
+
+public class CreateSemesterForm {
+
+    @NotEmpty
+    private String label;
+
+    public CreateSemesterForm() {
+    }
+
+    public CreateSemesterForm(String label) {
+        this.label = label;
+    }
+
+    @NotNull
+    public Semester toSemester() {
+        return new Semester(getLabel());
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CreateSemesterForm that = (CreateSemesterForm) o;
+
+        return label.equals(that.label);
+
+    }
+
+    @Override
+    public String toString() {
+        return "CreateSemesterForm{" +
+                "label='" + label + '\'' +
+                '}';
+    }
+}
