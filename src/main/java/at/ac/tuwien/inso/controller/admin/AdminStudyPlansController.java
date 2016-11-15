8fe7305da7e47f61aec0dff4e92fabe46846bcbe
Enri Miho <e0929003@student.tuwien.ac.at>
2016-11-16 00:31:23 +0100

    view and controller for list of all study plans.

--- src/main/java/at/ac/tuwien/inso/controller/admin/AdminStudyPlansController.java
+++ src/main/java/at/ac/tuwien/inso/controller/admin/AdminStudyPlansController.java
@@ -1,5 +1,8 @@
 package at.ac.tuwien.inso.controller.admin;
 
+import at.ac.tuwien.inso.entity.StudyPlan;
+import at.ac.tuwien.inso.service.StudyPlanService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,8 +10,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/admin/studyplans")
 public class AdminStudyPlansController {
 
+    @Autowired
+    private StudyPlanService studyPlanService;
+
     @GetMapping
     public String studyplans() {
         return "admin/studyplans";
+    }
+
+    @ModelAttribute("studyPlans")
+    private Iterable<StudyPlan> getStudyPlans() {
+        return studyPlanService.getAllStudyPlans();
     }
 }
