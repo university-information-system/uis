af8587a46f9c4402a464f1dccaa525b2a54afa7d
Enri Miho <e0929003@student.tuwien.ac.at>
2016-11-17 21:00:01 +0100

    view and controller for study plan creation.

--- src/main/java/at/ac/tuwien/inso/controller/admin/AdminStudyPlansController.java
+++ src/main/java/at/ac/tuwien/inso/controller/admin/AdminStudyPlansController.java
@@ -1,13 +1,16 @@
 package at.ac.tuwien.inso.controller.admin;
 
+import at.ac.tuwien.inso.controller.admin.forms.CreateStudyPlanForm;
 import at.ac.tuwien.inso.entity.StudyPlan;
 import at.ac.tuwien.inso.entity.SubjectForStudyPlan;
 import at.ac.tuwien.inso.service.StudyPlanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.*;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -35,6 +38,21 @@ public class AdminStudyPlansController {
         Iterable<SubjectForStudyPlan> subjectsForStudyPlan = studyPlanService.getSubjectsForStudyPlan(id);
         model.addAttribute("mandatory", StreamSupport.stream(subjectsForStudyPlan.spliterator(), false).filter(SubjectForStudyPlan::getMandatory).collect(Collectors.toList()));
         model.addAttribute("optional", StreamSupport.stream(subjectsForStudyPlan.spliterator(), false).filter(s -> !s.getMandatory()).collect(Collectors.toList()));
+        return "admin/studyplan-details";
+    }
+
+    @GetMapping("/create")
+    public String createStudyPlanView(CreateStudyPlanForm form, Model model) {
+        return "admin/create-studyplan";
+    }
+
+    @PostMapping("/create")
+    public String createStudyPlan(@Valid CreateStudyPlanForm form, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "admin/create-studyplan";
+        }
+        model.addAttribute("studyPlan",studyPlanService.create(form.toStudyPlan()));
+
         return "admin/studyplan-details";
     }
 }
