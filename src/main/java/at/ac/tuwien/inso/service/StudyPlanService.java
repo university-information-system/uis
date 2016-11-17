7f7612df68a58958d34617125df927106d6fff4c
Enri Miho <e0929003@student.tuwien.ac.at>
2016-11-17 17:50:19 +0100

    use methods for querying study plans and subjects (of a study plan) in service

--- src/main/java/at/ac/tuwien/inso/service/StudyPlanService.java
+++ src/main/java/at/ac/tuwien/inso/service/StudyPlanService.java
@@ -1,7 +1,10 @@
 package at.ac.tuwien.inso.service;
 
 import at.ac.tuwien.inso.entity.StudyPlan;
+import at.ac.tuwien.inso.entity.Subject;
+import at.ac.tuwien.inso.entity.SubjectForStudyPlan;
 import at.ac.tuwien.inso.repository.StudyPlanRepository;
+import at.ac.tuwien.inso.repository.SubjectForStudyPlanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +14,18 @@ public class StudyPlanService {
     @Autowired
     private StudyPlanRepository studyPlanRepository;
 
+    @Autowired
+    private SubjectForStudyPlanRepository subjectForStudyPlanRepository;
+
     public Iterable<StudyPlan> getAllStudyPlans() {
         return studyPlanRepository.findAll();
+    }
+
+    public StudyPlan getStudyPlanById(Long id) {
+        return studyPlanRepository.findStudyPlanById(id);
+    }
+
+    public Iterable<SubjectForStudyPlan> getSubjectsForStudyPlan(Long id) {
+        return subjectForStudyPlanRepository.findByStudyPlanIdOrderBySemesterRecommendation(id);
     }
 }
