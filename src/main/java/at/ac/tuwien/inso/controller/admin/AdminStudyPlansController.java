c2964fadfb44238bce5a2d869dc556a78a21a998
Enri Miho <e0929003@student.tuwien.ac.at>
2016-11-18 22:18:04 +0100

    add subjects to study plan.

--- src/main/java/at/ac/tuwien/inso/controller/admin/AdminStudyPlansController.java
+++ src/main/java/at/ac/tuwien/inso/controller/admin/AdminStudyPlansController.java
@@ -2,8 +2,11 @@ package at.ac.tuwien.inso.controller.admin;
 
 import at.ac.tuwien.inso.controller.admin.forms.CreateStudyPlanForm;
 import at.ac.tuwien.inso.entity.StudyPlan;
+import at.ac.tuwien.inso.entity.Subject;
 import at.ac.tuwien.inso.entity.SubjectForStudyPlan;
 import at.ac.tuwien.inso.service.StudyPlanService;
+import at.ac.tuwien.inso.service.SubjectService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.*;
 import org.springframework.ui.Model;
@@ -11,6 +14,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -20,6 +27,12 @@ public class AdminStudyPlansController {
 
     @Autowired
     private StudyPlanService studyPlanService;
+
+    @Autowired
+    private SubjectService subjectService;
+
+    private List<SubjectForStudyPlan> subjectsForStudyPlan;
+    private List<Subject> usedSubjects;
 
     @GetMapping
     public String studyplans() {
@@ -35,9 +48,12 @@ public class AdminStudyPlansController {
     private String getStudyPlan(@RequestParam(value = "id") Long id, Model model) {
         StudyPlan studyPlan = studyPlanService.getStudyPlanById(id);
         model.addAttribute("studyPlan", studyPlan);
-        Iterable<SubjectForStudyPlan> subjectsForStudyPlan = studyPlanService.getSubjectsForStudyPlan(id);
-        model.addAttribute("mandatory", StreamSupport.stream(subjectsForStudyPlan.spliterator(), false).filter(SubjectForStudyPlan::getMandatory).collect(Collectors.toList()));
-        model.addAttribute("optional", StreamSupport.stream(subjectsForStudyPlan.spliterator(), false).filter(s -> !s.getMandatory()).collect(Collectors.toList()));
+        this.subjectsForStudyPlan = StreamSupport.stream(studyPlanService.getSubjectsForStudyPlan(id).spliterator(), false).collect(Collectors.toList());
+        model.addAttribute("mandatory", subjectsForStudyPlan.stream().filter(SubjectForStudyPlan::getMandatory).collect(Collectors.toList()));
+        model.addAttribute("optional", subjectsForStudyPlan.stream().filter(s -> !s.getMandatory()).collect(Collectors.toList()));
+
+        //collect the subject objects for further use
+        this.usedSubjects = subjectsForStudyPlan.stream().map(SubjectForStudyPlan::getSubject).collect(Collectors.toList());
         return "admin/studyplan-details";
     }
 
@@ -55,4 +71,39 @@ public class AdminStudyPlansController {
 
         return "admin/studyplan-details";
     }
+
+    @PostMapping(value = "/addSubject", params = {"subjectJson", "studyPlanId", "semester"})
+    public String addSubjectToStudyPlan(
+            @RequestParam String subjectJson,
+            @RequestParam Long studyPlanId,
+            @RequestParam Integer semester,
+            @RequestParam Boolean mandatory) {
+
+        StudyPlan studyPlan = new StudyPlan();
+        studyPlan.setId(studyPlanId);
+        try {
+            Subject subject = new ObjectMapper().readValue(subjectJson, Subject.class);
+            studyPlanService.addSubjectToStudyPlan(new SubjectForStudyPlan(subject, studyPlan, mandatory, semester));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "redirect:/admin/studyplans/?id=" + studyPlanId;
+    }
+
+
+    @GetMapping(value = "/json/availableSubjects", params = "query")
+    @ResponseBody
+    public List<Subject> subjects(@RequestParam String query) {
+        List<Subject> subjects = new ArrayList<>();
+        Iterable<Subject> iterable = subjectService.searchForSubjects(query);
+        Iterator<Subject> iter = iterable.iterator();
+        while(iter.hasNext()) {
+            Subject subject = iter.next();
+            if(!usedSubjects.contains(subject)) {
+                subjects.add(subject);
+            }
+        }
+        return subjects;
+    }
+
 }
