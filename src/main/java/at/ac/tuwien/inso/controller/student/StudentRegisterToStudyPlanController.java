53d9ababe71b0c5bbf23b0bd9f99324440f83fb9
Michael Pazourek <e1225721@student.tuwien.ac.at>
2016-11-21 14:03:58 +0100

    fixed bugs. Feature #10852 works now

--- src/main/java/at/ac/tuwien/inso/controller/student/StudentRegisterToStudyPlanController.java
+++ src/main/java/at/ac/tuwien/inso/controller/student/StudentRegisterToStudyPlanController.java
@@ -1,23 +1,24 @@
 package at.ac.tuwien.inso.controller.student;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import at.ac.tuwien.inso.entity.Student;
 import at.ac.tuwien.inso.entity.StudyPlan;
-import at.ac.tuwien.inso.entity.UisUser;
+import at.ac.tuwien.inso.entity.StudyPlanRegistration;
 import at.ac.tuwien.inso.service.StudentService;
 import at.ac.tuwien.inso.service.StudyPlanService;
 
 @Controller
 @RequestMapping("/admin/registerToStudyplan")
-public class StudentRegisterToStudyPlan {
+public class StudentRegisterToStudyPlanController {
 
 	@Autowired
 	private StudyPlanService studyPlanService;
@@ -27,38 +28,36 @@ public class StudentRegisterToStudyPlan {
 	
 	@GetMapping
     public String registerStudent(@RequestParam Long studentId, @RequestParam Long studyPlanId) {
-    	System.out.println("hier geht er rein");
-    	System.out.println("id"+studentId);
     	StudyPlan studyPlan = studyPlanService.getStudyPlanById(studyPlanId);
     	Student student = studentService.findStudentById(studentId);
     	
     	studentService.registerStudentToStudyPlan(student, studyPlan);
     	    	
-    	return "admin/users/"+studentId;
+    	return "redirect:/admin/users/"+student.getId();
     }
 	
-    @GetMapping(params = "studentId")
-    public String registerStudentView(@RequestParam Long studentId, Model model) {
-    	UisUser student = studentService.findStudentById(studentId);
+    @GetMapping(params = "studentToAddId")
+    public String registerStudentView(@RequestParam Long studentToAddId, Model model) {
+    	Student student = studentService.findStudentById(studentToAddId);
     	
     	model.addAttribute("user", student);
-    	
     	model.addAttribute("test", "testString");
     	
-        //model.addAttribute("studyPlans", studyPlanService.getAllStudyPlans());
+    	List<StudyPlan> toShow = new ArrayList<StudyPlan>();
+    	for(StudyPlan sp:studyPlanService.getAllStudyPlans()){
+    		boolean error = false;
+    		for(StudyPlanRegistration studentSp : student.getStudyplans()){
+    			if(sp.equals(studentSp.getStudyplan())){
+	    			error = true;
+	    		}
+    		}
+    		if(!error){
+    			toShow.add(sp);
+    		}
+    	}
+    	
+        model.addAttribute("studyPlans", toShow);
         
     	return "admin/addStudyPlanToStudent";
     }
-    
-    @ModelAttribute("studyPlans")
-    private Iterable<StudyPlan> getStudyPlans() {
-    	System.out.println("get all studyplans");
-        return studyPlanService.getAllStudyPlans();
-    }
-    
-    /*@GetMapping
-    private String registerStudent2() {
-    	System.out.println("hier geht er rein2");
-    	return "null";
-    }*/
 }
