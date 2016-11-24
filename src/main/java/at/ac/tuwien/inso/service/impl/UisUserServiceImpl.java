108a305471e250cad525b25ac381e20b05a4bc5e
Enri Miho <e0929003@student.tuwien.ac.at>
2016-11-24 23:26:23 +0100

    use BusinessObjectNotFoundException

--- src/main/java/at/ac/tuwien/inso/service/impl/UisUserServiceImpl.java
+++ src/main/java/at/ac/tuwien/inso/service/impl/UisUserServiceImpl.java
@@ -1,13 +1,13 @@
 package at.ac.tuwien.inso.service.impl;
 
 import at.ac.tuwien.inso.entity.*;
+import at.ac.tuwien.inso.exception.BusinessObjectNotFoundException;
 import at.ac.tuwien.inso.repository.*;
 import at.ac.tuwien.inso.service.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
 import org.springframework.transaction.annotation.*;
 
-import javax.persistence.*;
 import java.util.*;
 
 @Service
@@ -27,7 +27,7 @@ public class UisUserServiceImpl implements UisUserService {
     public UisUser findOne(long id) {
         UisUser user = uisUserRepository.findOne(id);
         if (user == null) {
-            throw new EntityNotFoundException("Invalid user id: " + id);
+            throw new BusinessObjectNotFoundException("Invalid user id: " + id);
         }
 
         return user;
