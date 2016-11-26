4665d6f909361cd703df78009fbb0a12c4610f53
Damjan Gjurovski <e1129609@student.tuwien.ac.at>
2016-11-26 02:53:40 +0100

    Add tests for UserAccountService security

---
+++ src/test/java/at/ac/tuwien/inso/method_security_tests/UserAccountServiceSecurityTests.java
@@ -0,0 +1,38 @@
+package at.ac.tuwien.inso.method_security_tests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import at.ac.tuwien.inso.service.UserAccountService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+public class UserAccountServiceSecurityTests {
+
+
+    @Autowired
+    private UserAccountService userAccountService;
+
+    @Test(expected = AuthenticationCredentialsNotFoundException.class)
+    public void getCurrentLoggedInUserNotAuthenticated() {
+        userAccountService.getCurrentLoggedInUser();
+    }
+
+    @Test(expected = UsernameNotFoundException.class)
+    @WithMockUser
+    public void getCurrentLoggedInUserAuthenticated() {
+        userAccountService.getCurrentLoggedInUser();
+    }
+}
