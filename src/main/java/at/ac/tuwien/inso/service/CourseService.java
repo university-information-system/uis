c035b0cc35ef8c289feb4069933e93852d7e92c1
Damjan Gjurovski <e1129609@student.tuwien.ac.at>
2016-11-26 01:01:07 +0100

    Add method security annotations to courseService

--- src/main/java/at/ac/tuwien/inso/service/CourseService.java
+++ src/main/java/at/ac/tuwien/inso/service/CourseService.java
@@ -1,5 +1,7 @@
 package at.ac.tuwien.inso.service;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+
 import at.ac.tuwien.inso.controller.lecturer.forms.*;
 import at.ac.tuwien.inso.entity.*;
 
@@ -7,13 +9,19 @@ import javax.validation.constraints.*;
 import java.util.*;
 
 public interface CourseService {
+
+    @PreAuthorize("isAuthenticated()")
     List<Course> findCourseForCurrentSemesterWithName(@NotNull String name);
 
+    @PreAuthorize("isAuthenticated()")
     List<Course> findCoursesForCurrentSemesterForLecturer(Lecturer lecturer);
 
+    @PreAuthorize("hasAnyRole('ROLE_LECTURER', 'ROLE_ADMIN')")
     Course saveCourse(AddCourseForm form);
 
+    @PreAuthorize("isAuthenticated()")
     Course findOne(Long id);
 
+    @PreAuthorize("hasRole('STUDENT')")
     boolean registerStudentForCourse(Course course);
 }
