267ccb52105a18bf4f6cb5dc34b3486541973325
Damjan Gjurovski <e1129609@student.tuwien.ac.at>
2016-11-26 02:19:01 +0100

    Add StudyPlanService method security

--- src/main/java/at/ac/tuwien/inso/service/StudyPlanService.java
+++ src/main/java/at/ac/tuwien/inso/service/StudyPlanService.java
@@ -1,21 +1,29 @@
 package at.ac.tuwien.inso.service;
 
 import at.ac.tuwien.inso.entity.*;
+
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
 
 public interface StudyPlanService {
 
+    @PreAuthorize("hasRole('ADMIN')")
     StudyPlan create(StudyPlan studyPlan);
 
+    @PreAuthorize("isAuthenticated()")
     List<StudyPlan> findAll();
 
+    @PreAuthorize("isAuthenticated()")
     StudyPlan findOne(Long id);
 
+    @PreAuthorize("isAuthenticated()")
     List<SubjectForStudyPlan> getSubjectsForStudyPlan(Long id);
 
+    @PreAuthorize("hasRole('ADMIN')")
     void addSubjectToStudyPlan(SubjectForStudyPlan subjectForStudyPlan);
 
+    @PreAuthorize("isAuthenticated()")
     List<Subject> getAvailableSubjectsForStudyPlan(Long id, String query);
 }
