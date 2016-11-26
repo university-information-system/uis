d50a9ed1fd0039a7a457a9293b6917e30bd734ed
Damjan Gjurovski <e1129609@student.tuwien.ac.at>
2016-11-26 02:30:23 +0100

    Add tests for method security for SubjectService

---
+++ src/test/java/at/ac/tuwien/inso/method_security_tests/SubjectServiceSecurityTests.java
@@ -0,0 +1,87 @@
+package at.ac.tuwien.inso.method_security_tests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+
+import at.ac.tuwien.inso.entity.Subject;
+import at.ac.tuwien.inso.service.SubjectService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+public class SubjectServiceSecurityTests {
+
+    @Autowired
+    private SubjectService subjectService;
+
+
+    @Test(expected = AuthenticationCredentialsNotFoundException.class)
+    public void createNotAuthenticated() {
+        subjectService.create(new Subject());
+    }
+
+    @Test(expected = AccessDeniedException.class)
+    @WithMockUser(roles = "STUDENT")
+    public void createNotAuthenticatedAsStudent() {
+        subjectService.create(new Subject());
+    }
+
+    @Test(expected = AccessDeniedException.class)
+    @WithMockUser(roles = "LECTURER")
+    public void createNotAuthenticatedAsLecturer() {
+        subjectService.create(new Subject());
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void createNotAuthenticatedAsAdmin() {
+        subjectService.create(new Subject("ASE", BigDecimal.ONE));
+    }
+
+    @Test(expected = AuthenticationCredentialsNotFoundException.class)
+    public void findAllNotAuthenticated() {
+        subjectService.findAll();
+    }
+
+    @Test
+    @WithMockUser
+    public void findAllAuthenticated() {
+        subjectService.findAll();
+    }
+
+    @Test(expected = AuthenticationCredentialsNotFoundException.class)
+    public void findOneNotAuthenticated() {
+        subjectService.findOne(Long.valueOf(1));
+    }
+
+    @Test
+    @WithMockUser
+    public void findOneAuthenticated() {
+        subjectService.findOne(Long.valueOf(1));
+    }
+
+    @Test(expected = AuthenticationCredentialsNotFoundException.class)
+    public void searchForSubjectsNotAuthenticated() {
+        subjectService.searchForSubjects("ASE");
+    }
+
+    @Test
+    @WithMockUser
+    public void searchForSubjectsAuthenticated() {
+        subjectService.searchForSubjects("ASE");
+    }
+
+}
