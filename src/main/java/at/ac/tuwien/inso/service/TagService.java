b5e0501c60d7f5ac9770997074e1c99dd0721ef9
Damjan Gjurovski <e1129609@student.tuwien.ac.at>
2016-11-26 02:33:48 +0100

    Add method security for TagService

--- src/main/java/at/ac/tuwien/inso/service/TagService.java
+++ src/main/java/at/ac/tuwien/inso/service/TagService.java
@@ -1,10 +1,13 @@
 package at.ac.tuwien.inso.service;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+
 import at.ac.tuwien.inso.entity.*;
 
 import java.util.*;
 
 public interface TagService {
 
+    @PreAuthorize("isAuthenticated()")
     List<Tag> findAll();
 }
