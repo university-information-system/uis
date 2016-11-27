cd822667db85f4673d9888d64581bfcec783970c
Martin Stücklschwaiger <martin.st@wolke7.net>
2016-11-27 23:49:16 +0100

    Add LecturerRepository.findAll()

--- src/main/java/at/ac/tuwien/inso/repository/LecturerRepository.java
+++ src/main/java/at/ac/tuwien/inso/repository/LecturerRepository.java
@@ -4,8 +4,12 @@ import at.ac.tuwien.inso.entity.*;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.*;
 
+import java.util.List;
+
 public interface LecturerRepository extends CrudRepository<Lecturer, Long> {
 
     @Query("select l from Lecturer l where ACCOUNT_ID = ?1")
     Lecturer findLecturerByAccountId(Long id);
+
+    public List<Lecturer> findAll();
 }
