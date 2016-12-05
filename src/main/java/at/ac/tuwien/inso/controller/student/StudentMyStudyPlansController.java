488bc21534487edc94951382ed3905a24f24b4cd
Enri Miho <e0929003@student.tuwien.ac.at>
2016-12-05 12:32:18 +0100

    show study plans of a student.

--- src/main/java/at/ac/tuwien/inso/controller/student/StudentMyStudyPlansController.java
+++ src/main/java/at/ac/tuwien/inso/controller/student/StudentMyStudyPlansController.java
@@ -1,5 +1,10 @@
 package at.ac.tuwien.inso.controller.student;
 
+import at.ac.tuwien.inso.entity.Student;
+import at.ac.tuwien.inso.entity.StudyPlanRegistration;
+import at.ac.tuwien.inso.service.StudentService;
+import at.ac.tuwien.inso.service.UserAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,8 +12,22 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/student/my-studyplans")
 public class StudentMyStudyPlansController {
 
+    @Autowired
+    private UserAccountService userAccountService;
+
+    @Autowired
+    private StudentService studentService;
+
     @GetMapping
-    public String studyplans() {
+    public String listStudyPlansForStudent() {
         return "/student/my-studyplans";
     }
+
+    @ModelAttribute("studyPlanRegistrations")
+    private Iterable<StudyPlanRegistration> getStudyPlans() {
+        Student student = studentService.findOne(userAccountService.getCurrentLoggedInUser());
+        return student.getStudyplans();
+    }
+
+
 }
