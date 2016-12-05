c48a704a0118cc08d266c6399be22760ce7373d6
Enri Miho <e0929003@student.tuwien.ac.at>
2016-12-05 11:37:10 +0100

    show all study plans in student view.

--- src/main/java/at/ac/tuwien/inso/controller/student/StudentAllStudyPlansController.java
+++ src/main/java/at/ac/tuwien/inso/controller/student/StudentAllStudyPlansController.java
@@ -1,5 +1,8 @@
 package at.ac.tuwien.inso.controller.student;
 
+import at.ac.tuwien.inso.entity.StudyPlan;
+import at.ac.tuwien.inso.service.StudyPlanService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,8 +10,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/student/all-studyplans")
 public class StudentAllStudyPlansController {
 
+    @Autowired
+    private StudyPlanService studyPlanService;
+
     @GetMapping
-    public String studyplans() {
+    public String listStudyPlans() {
         return "/student/all-studyplans";
+    }
+
+    @ModelAttribute("studyPlans")
+    private Iterable<StudyPlan> getStudyPlans() {
+        return studyPlanService.findAll();
     }
 }
