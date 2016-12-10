8ca8cb8e8657878b0eabe5fbb515e0ac76d9dd4f
Martin Stücklschwaiger <martin.st@wolke7.net>
2016-12-10 23:20:19 +0100

    Add logger imports

--- src/main/java/at/ac/tuwien/inso/service/impl/SubjectServiceImpl.java
+++ src/main/java/at/ac/tuwien/inso/service/impl/SubjectServiceImpl.java
@@ -1,5 +1,7 @@
 package at.ac.tuwien.inso.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
