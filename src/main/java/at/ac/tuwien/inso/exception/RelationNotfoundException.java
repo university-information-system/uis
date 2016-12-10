854ecd4b8e687011514b2f0f67e630a9bcbf41a1
Martin Stücklschwaiger <martin.st@wolke7.net>
2016-12-10 23:18:37 +0100

    Add RelationNotFoundException

---
+++ src/main/java/at/ac/tuwien/inso/exception/RelationNotfoundException.java
@@ -0,0 +1,12 @@
+package at.ac.tuwien.inso.exception;
+
+public class RelationNotfoundException extends RuntimeException {
+
+    public RelationNotfoundException() {
+        super();
+    }
+
+    public RelationNotfoundException(String s) {
+        super(s);
+    }
+}
