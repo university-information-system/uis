53c2ee68de0db883b0ba9ecfe1b7b3a1bd7f5e1b
Martin Stücklschwaiger <martin.st@wolke7.net>
2016-12-10 15:11:38 +0100

    Fix AddLecturersToSubjectForm

--- src/main/java/at/ac/tuwien/inso/controller/admin/forms/AddLecturersToSubjectForm.java
+++ src/main/java/at/ac/tuwien/inso/controller/admin/forms/AddLecturersToSubjectForm.java
@@ -1,58 +1,33 @@
 package at.ac.tuwien.inso.controller.admin.forms;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-
-import javax.validation.constraints.NotNull;
-
-import at.ac.tuwien.inso.entity.Lecturer;
-import at.ac.tuwien.inso.service.LecturerService;
-import at.ac.tuwien.inso.service.SubjectService;
-
-
 public class AddLecturersToSubjectForm {
 
     public AddLecturersToSubjectForm() {
     }
 
-    public AddLecturersToSubjectForm(Long id) {
-        this.id = id;
+    public AddLecturersToSubjectForm(Long lecturerId) {
+        this.lecturerId = lecturerId;
     }
 
-    private Long id;
+    private Long lecturerId;
 
-    public Long getId() {
-        return id;
+    public Long getLecturerId() {
+        return lecturerId;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setLecturerId(Long lecturerId) {
+        this.lecturerId = lecturerId;
     }
 
     public Long toLecturerId() {
-        return id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        AddLecturersToSubjectForm that = (AddLecturersToSubjectForm) o;
-
-        return id != null ? id.equals(that.id) : that.id == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        return id != null ? id.hashCode() : 0;
+        return lecturerId;
     }
 
     @Override
     public String toString() {
         return "AddLecturersToSubjectForm{" +
-                "id=" + id +
+                "lecturerId=" + lecturerId +
                 '}';
     }
 }
