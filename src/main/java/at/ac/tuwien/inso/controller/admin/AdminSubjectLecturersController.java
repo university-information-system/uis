2ed1cc3432c9b9ab6ce6ffbb56fa4115b80f1018
Martin Stücklschwaiger <martin.st@wolke7.net>
2016-12-10 15:26:39 +0100

    remove unused import

--- src/main/java/at/ac/tuwien/inso/controller/admin/AdminSubjectLecturersController.java
+++ src/main/java/at/ac/tuwien/inso/controller/admin/AdminSubjectLecturersController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
