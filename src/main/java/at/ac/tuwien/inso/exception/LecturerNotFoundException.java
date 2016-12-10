f694d8fb72a8ca3d545d56e96957e703779bedd9
Martin Stücklschwaiger <martin.st@wolke7.net>
2016-12-10 23:19:42 +0100

    Implement LecturerNotFoundException

--- src/main/java/at/ac/tuwien/inso/exception/LecturerNotFoundException.java
+++ src/main/java/at/ac/tuwien/inso/exception/LecturerNotFoundException.java
@@ -1,5 +1,12 @@
 package at.ac.tuwien.inso.exception;
 
 
-public class LecturerNotFoundException extends Throwable {
+public class LecturerNotFoundException extends BusinessObjectNotFoundException {
+
+    public LecturerNotFoundException() {
+    }
+
+    public LecturerNotFoundException(String s) {
+        super(s);
+    }
 }
