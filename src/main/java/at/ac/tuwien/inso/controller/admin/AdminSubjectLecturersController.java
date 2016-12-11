350fcd844c244a48a7065561e9bdf44f679522bb
Martin Stücklschwaiger <martin.st@wolke7.net>
2016-12-11 20:23:15 +0100

    Remove unused imports

--- src/main/java/at/ac/tuwien/inso/controller/admin/AdminSubjectLecturersController.java
+++ src/main/java/at/ac/tuwien/inso/controller/admin/AdminSubjectLecturersController.java
@@ -2,8 +2,6 @@ package at.ac.tuwien.inso.controller.admin;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
