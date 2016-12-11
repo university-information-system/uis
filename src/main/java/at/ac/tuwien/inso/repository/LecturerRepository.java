aad9653d4626ebd295b76c1fa79217cf8f139c25
Martin Stücklschwaiger <martin.st@wolke7.net>
2016-12-11 22:23:32 +0100

    Replace findAll with complicated search operation

--- src/main/java/at/ac/tuwien/inso/repository/LecturerRepository.java
+++ src/main/java/at/ac/tuwien/inso/repository/LecturerRepository.java
@@ -12,7 +12,11 @@ public interface LecturerRepository extends CrudRepository<Lecturer, Long> {
     @Query("select l from Lecturer l where ACCOUNT_ID = ?1")
     Lecturer findLecturerByAccountId(Long id);
 
-    public List<Lecturer> findAll();
+
+    public List<Lecturer> findAllByIdentificationNumberLikeIgnoreCaseOrNameLikeIgnoreCase(
+            String identificationNumber,
+            String name
+    );
 
     public Lecturer findById(Long id);
 }
