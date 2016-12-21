be39b24cf50a87dfae45bf05540a484a9b216d00
Enri Miho <e0929003@student.tuwien.ac.at>
2016-12-21 16:58:43 +0100

    integration testing

--- src/test/java/at/ac/tuwien/inso/integration_tests/StudentFeedbackTests.java
+++ src/test/java/at/ac/tuwien/inso/integration_tests/StudentFeedbackTests.java
@@ -47,6 +47,12 @@ public class StudentFeedbackTests {
     private SemesterRepository semesterRepository;
     @Autowired
     private FeedbackRepository feedbackRepository;
+    @Autowired
+    private StudentRepository studentRepository;
+    @Autowired
+    private GradeRepository gradeRepository;
+    @Autowired
+    private LecturerRepository lecturerRepository;
 
     private Student student;
     private List<Course> courses;
@@ -158,6 +164,59 @@ public class StudentFeedbackTests {
         ).andExpect(
                 status().isBadRequest()
         );
+    }
+
+    @Test
+    public void lecturerShouldSeeFeedback() throws Exception {
+
+        // given course ase held by lecturer1 and students student1 and student2 with grades in ase
+        Lecturer lecturer1 = lecturerRepository.save(new Lecturer("l0002", "Lecturer 1", "email", new UserAccount("lecturer1", "pass", Role.LECTURER)));
+        Student st1 = studentRepository.save(new Student("st1", "Student2", "st1@ude.nt", new UserAccount("st1", "pass", Role.STUDENT)));
+        Student st2 = studentRepository.save(new Student("st2", "Student3", "st2@ude.nt", new UserAccount("st2", "pass", Role.STUDENT)));
+        Subject ase = subjectRepository.save(new Subject("ASE", new BigDecimal(6.0)));
+        Semester ws2016 = semesterRepository.save(new Semester("WS2016"));
+        Course aseWS2016 = courseRepository.save(new Course(ase, ws2016).addStudents(st1,st2));
+        gradeRepository.save(new Grade(aseWS2016, lecturer1, st1, Mark.EXCELLENT));
+        gradeRepository.save(new Grade(aseWS2016, lecturer1, st2, Mark.GOOD));
+
+        // when student1 and student2 give feedback
+        Feedback feedback1 = new Feedback(
+                st1,
+                aseWS2016,
+                Feedback.Type.DISLIKE, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec enim ligula. " +
+                "Sed eget posuere tellus. Aenean fermentum maximus tempor. Ut ultricies dapibus nulla vitae mollis. " +
+                "Suspendisse a nunc nisi. Sed ut sapien eu odio sodales laoreet eu ac turpis. " +
+                "In id sapien id ante sollicitudin consectetur at laoreet mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+                "Suspendisse quam sem, ornare eget pellentesque sit amet, tincidunt id metus. Sed scelerisque neque sed laoreet elementum. " +
+                "Integer eros neque, vulputate a hendrerit at, ullamcorper in orci. Donec sit amet risus hendrerit, hendrerit magna non, dapibus nibh. " +
+                "Suspendisse sed est feugiat, dapibus ante non, aliquet neque. Cras magna sapien, pharetra ut ante ut, malesuada hendrerit erat. " +
+                "Mauris fringilla mattis dapibus. Nullam iaculis nunc in tortor gravida, id tempor justo elementum.");
+        Feedback feedback2 = new Feedback(
+                st2,
+                aseWS2016,
+                Feedback.Type.LIKE, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec enim ligula. " +
+                "Sed eget posuere tellus. Aenean fermentum maximus tempor. Ut ultricies dapibus nulla vitae mollis. " +
+                "Suspendisse a nunc nisi. Sed ut sapien eu odio sodales laoreet eu ac turpis. " +
+                "In id sapien id ante sollicitudin consectetur at laoreet mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+                "Suspendisse quam sem, ornare eget pellentesque sit amet, tincidunt id metus. Sed scelerisque neque sed laoreet elementum. " +
+                "Integer eros neque, vulputate a hendrerit at, ullamcorper in orci. Donec sit amet risus hendrerit, hendrerit magna non, dapibus nibh. " +
+                "Suspendisse sed est feugiat, dapibus ante non, aliquet neque. Cras magna sapien, pharetra ut ante ut, malesuada hendrerit erat. " +
+                "Mauris fringilla mattis dapibus. Nullam iaculis nunc in tortor gravida, id tempor justo elementum.");
+
+        feedbackRepository.save(asList(feedback1, feedback2));
+
+        // the lecturer should see the feedback
+        mockMvc.perform(
+                get("/lecturer/course-details/feedback")
+                        .param("courseId", aseWS2016.getId().toString())
+                        .with(user("lecturer1").roles(Role.LECTURER.name()))
+                        .with(csrf())
+        ).andExpect(
+                model().attribute("course", aseWS2016)
+        ).andExpect(
+                model().attribute("feedbacks", asList(feedback1, feedback2))
+        );
+
     }
 
     private String tooLongSuggestions() {
