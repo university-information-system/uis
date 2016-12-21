f50cb6ab0bddb8ef1dd4d7e6a0eb16c874d7646f
Enri Miho <e0929003@student.tuwien.ac.at>
2016-12-21 16:33:35 +0100

    get course feedback in controller.

--- src/main/java/at/ac/tuwien/inso/controller/lecturer/LecturerCourseDetailsController.java
+++ src/main/java/at/ac/tuwien/inso/controller/lecturer/LecturerCourseDetailsController.java
@@ -2,6 +2,7 @@ package at.ac.tuwien.inso.controller.lecturer;
 
 import at.ac.tuwien.inso.entity.Course;
 import at.ac.tuwien.inso.service.CourseService;
+import at.ac.tuwien.inso.service.FeedbackService;
 import at.ac.tuwien.inso.service.GradeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,6 +20,9 @@ public class LecturerCourseDetailsController {
 
     @Autowired
     private GradeService gradeService;
+
+    @Autowired
+    private FeedbackService feedbackService;
 
     @GetMapping
     private String getCourseDetails(@RequestParam("courseId") Long courseId, Model model) {
@@ -40,5 +44,13 @@ public class LecturerCourseDetailsController {
         model.addAttribute("course", course);
         model.addAttribute("grades", gradeService.getGradesForCourseOfLoggedInLecturer(courseId));
         return "lecturer/issued-grades";
+    }
+
+    @GetMapping("feedback")
+    private String getCourseFeedback(@RequestParam("courseId") Long courseId, Model model) {
+        Course course = courseService.findOne(courseId);
+        model.addAttribute("course", course);
+        model.addAttribute("feedbacks", feedbackService.findFeedbackForCourse(courseId));
+        return "lecturer/course-feedback";
     }
 }
