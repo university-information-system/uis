1536569ef6334febeaee33de8abdef7867147e39
Enri Miho <e0929003@student.tuwien.ac.at>
2016-12-30 18:31:52 +0100

    show all study plans.

--- src/main/java/at/ac/tuwien/inso/controller/lecturer/LecturerStudyplansController.java
+++ src/main/java/at/ac/tuwien/inso/controller/lecturer/LecturerStudyplansController.java
@@ -1,14 +1,54 @@
 package at.ac.tuwien.inso.controller.lecturer;
 
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import at.ac.tuwien.inso.entity.StudyPlan;
+import at.ac.tuwien.inso.entity.SubjectForStudyPlan;
+import at.ac.tuwien.inso.service.StudyPlanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/lecturer/studyplans")
 public class LecturerStudyplansController {
 
+    @Autowired
+    private StudyPlanService studyPlanService;
+
     @GetMapping
-    public String studyplans() {
-        return "lecturer/studyplans";
+    public String listStudyPlans() {
+        return "/lecturer/studyplans";
+    }
+
+    @ModelAttribute("studyPlans")
+    private Iterable<StudyPlan> getStudyPlans() {
+        return studyPlanService.findAll();
+    }
+
+    @GetMapping(params = "id")
+    private String getStudyplanDetailsView(@RequestParam(value = "id") Long id, Model model) {
+        StudyPlan studyPlan = studyPlanService.findOne(id);
+
+        List<SubjectForStudyPlan> subjectsForStudyPlan = studyPlanService.getSubjectsForStudyPlan(id);
+        List<SubjectForStudyPlan> mandatory = subjectsForStudyPlan
+                .stream()
+                .filter(SubjectForStudyPlan::getMandatory)
+                .collect(Collectors.toList());
+        List<SubjectForStudyPlan> optional = subjectsForStudyPlan
+                .stream()
+                .filter(s -> !s.getMandatory())
+                .collect(Collectors.toList());
+
+        model.addAttribute("studyPlan", studyPlan);
+        model.addAttribute("mandatory", mandatory);
+        model.addAttribute("optional", optional);
+
+        return "student/studyplan-details";
     }
 }
