59762bf2ce469f73afed0f0b056c50182664a9aa
Michael Pazourek <e1225721@student.tuwien.ac.at>
2017-01-05 00:49:38 +0100

    added service test class for subject service tests

---
+++ src/test/java/at/ac/tuwien/inso/service_tests/SubjectServiceTests.java
@@ -0,0 +1,32 @@
+package at.ac.tuwien.inso.service_tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SubjectServiceTests {
+
+	
+	@Test
+	public void testRemoveWithValidInput(){
+		fail();
+	}
+	
+	@Test
+	public void testRemoveWithNullInput(){
+		
+	}
+	
+	@Test
+	public void testRemoveWithNonExistingInput(){
+		
+	}
+	
+	@Test
+	public void testRemoveWithContaingCoursesInput(){
+		
+	}
+}
