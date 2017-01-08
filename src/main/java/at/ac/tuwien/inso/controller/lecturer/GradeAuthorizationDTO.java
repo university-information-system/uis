d9380662b9a8abdd60ec66d2ada461c626c6d07d
Damjan Gjurovski <e1129609@student.tuwien.ac.at>
2017-01-08 05:04:17 +0100

    Add DTO for two factor authentication when grading

---
+++ src/main/java/at/ac/tuwien/inso/controller/lecturer/GradeAuthorizationDTO.java
@@ -0,0 +1,38 @@
+package at.ac.tuwien.inso.controller.lecturer;
+
+import at.ac.tuwien.inso.entity.Grade;
+
+public class GradeAuthorizationDTO {
+    //TODO: MOVE TO DTOs
+
+    private Grade grade;
+    private String authCode;
+
+    public GradeAuthorizationDTO() {
+    }
+
+    public GradeAuthorizationDTO(Grade grade) {
+        this.grade = grade;
+    }
+
+    public GradeAuthorizationDTO(Grade grade, String code) {
+        this.grade = grade;
+        this.authCode = code;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Grade grade) {
+        this.grade = grade;
+    }
+
+    public String getAuthCode() {
+        return authCode;
+    }
+
+    public void setAuthCode(String authCode) {
+        this.authCode = authCode;
+    }
+}
