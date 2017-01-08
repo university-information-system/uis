810b4afed7407076951c5bf796ba550e1328b9a3
Damjan Gjurovski <e1129609@student.tuwien.ac.at>
2017-01-08 05:08:16 +0100

    Add QR code generation for two factor authentification

--- src/main/java/at/ac/tuwien/inso/service/LecturerService.java
+++ src/main/java/at/ac/tuwien/inso/service/LecturerService.java
@@ -4,6 +4,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 
 import at.ac.tuwien.inso.entity.*;
 
+import java.io.UnsupportedEncodingException;
 import java.util.*;
 
 public interface LecturerService {
@@ -16,4 +17,7 @@ public interface LecturerService {
 
     @PreAuthorize("isAuthenticated()")
     List<Subject> findSubjectsFor(Lecturer lecturer);
+
+    @PreAuthorize("hasRole('ADMIN')")
+    String generateQRUrl(Lecturer lecturer) throws UnsupportedEncodingException;
 }
