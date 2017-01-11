813c4e20e6a0eab67ec317e039a358b804052483
Martin Stücklschwaiger <martin.st@wolke7.net>
2017-01-12 00:22:34 +0100

    Refactor SemesterDto

--- src/main/java/at/ac/tuwien/inso/dto/SemesterDto.java
+++ src/main/java/at/ac/tuwien/inso/dto/SemesterDto.java
@@ -1,19 +1,27 @@
 package at.ac.tuwien.inso.dto;
 
+import java.util.Objects;
+
 import at.ac.tuwien.inso.entity.Semester;
+import at.ac.tuwien.inso.entity.SemesterType;
 
 //finished transformation on 8.1.
 public class SemesterDto {
 
 	private Long id;
 
-	private String label;
+	private int year;
 
+	private SemesterType type;
 
-    public SemesterDto(String label) {
-        this.label = label;
-    }
-	
+	public SemesterDto() {
+	}
+
+	public SemesterDto(int year, SemesterType type) {
+		this.year = year;
+		this.type = type;
+	}
+
 	public Long getId() {
 		return id;
 	}
@@ -23,39 +31,47 @@ public class SemesterDto {
 	}
 
 	public String getLabel() {
-		return label;
+		return getYear() + " " + getType();
 	}
 
-	public void setLabel(String label) {
-		this.label = label;
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public SemesterType getType() {
+		return type;
+	}
+
+	public void setType(SemesterType type) {
+		this.type = type;
 	}
 
 	@Override
 	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
 
-		SemesterDto semester = (SemesterDto) o;
+		SemesterDto that = (SemesterDto) o;
 
-		if (id != null ? !id.equals(semester.id) : semester.id != null) return false;
-		return label != null ? label.equals(semester.label) : semester.label == null;
-
+		return getYear() == that.getYear() && Objects.equals(getId(), that.getId()) && getType() == that.getType();
 	}
 
 	@Override
-	public String toString() {
-		return "SemesterDto{" +
-				"id=" + id +
-				", label='" + label + '\'' +
-				'}';
-	}	
-	
+	public int hashCode() {
+		return Objects.hash(getId(), getYear(), getType());
+	}
+
 	/**
 	 * sets the label to a new Semester instance. The Id will not be persisted.
 	 * @return
 	 */
 	public Semester toEntity(){
-		Semester semesterEntity = new Semester(label);
+		Semester semesterEntity = new Semester(year, type);
 		semesterEntity.setId(id);
 		return semesterEntity;
 	}
