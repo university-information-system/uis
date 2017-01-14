65648cd669b57197c94937253e1fc4805b3d16fb
Martin Stücklschwaiger <martin.st@wolke7.net>
2017-01-15 00:01:16 +0100

    Test new methods

--- src/test/java/at/ac/tuwien/inso/dto_tests/SemesterDtoTests.java
+++ src/test/java/at/ac/tuwien/inso/dto_tests/SemesterDtoTests.java
@@ -7,6 +7,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import at.ac.tuwien.inso.dto.SemesterDto;
 import at.ac.tuwien.inso.entity.SemesterType;
 
@@ -44,4 +47,76 @@ public class SemesterDtoTests {
 	public void testEqualsNoId() {
 		assertFalse(getWS2016().equals(getWS2016WithoutId()));
 	}
+
+	@Test
+	public void testIsStartInPast() {
+        Calendar before = new GregorianCalendar(2000, 1, 1);
+        Calendar after = new GregorianCalendar(2020, 3, 3);
+
+        SemesterDto semester = new SemesterDto(2010, SemesterType.SummerSemester);
+
+        assertFalse(semester.isStartInPast(before));
+        assertTrue(semester.isStartInPast(after));
+    }
+
+	@Test
+	public void testCalculateCurrentSemesterSS() {
+		Calendar currentDay = new GregorianCalendar(2016, 06, 06);
+
+		SemesterDto expected = new SemesterDto(2016, SemesterType.SummerSemester);
+
+		SemesterDto actual = SemesterDto.calculateCurrentSemester(currentDay);
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testCalculateCurrentSemesterWS() {
+		Calendar currentDay = new GregorianCalendar(2000, 01, 07);
+
+		SemesterDto expected = new SemesterDto(1999, SemesterType.WinterSemester);
+
+		SemesterDto actual = SemesterDto.calculateCurrentSemester(currentDay);
+
+		assertEquals(expected, actual);
+	}
+
+    @Test
+    public void testIsCurrent() {
+        Calendar now = new GregorianCalendar(2016, 1, 1);
+
+        SemesterDto semester = new SemesterDto(2015, SemesterType.WinterSemester);
+
+        assertTrue(semester.isCurrent(now));
+    }
+
+    @Test
+    public void testNotIsCurrent() {
+        Calendar now = new GregorianCalendar(2000, 7, 7);
+
+        SemesterDto semester = new SemesterDto(2000, SemesterType.WinterSemester);
+
+        assertFalse(semester.isCurrent(now));
+    }
+
+    @Test
+    public void testNotIsCurrentOtherYear() {
+        Calendar now = new GregorianCalendar(2000, 7, 7);
+
+        SemesterDto semester = new SemesterDto(2010, SemesterType.SummerSemester);
+
+        assertFalse(semester.isCurrent(now));
+    }
+
+    @Test
+    public void testNextPossibleSemester() {
+	    SemesterDto semester = new SemesterDto(2016, SemesterType.WinterSemester);
+
+	    SemesterDto expected = new SemesterDto(2017, SemesterType.SummerSemester);
+
+	    SemesterDto actual = semester.nextSemester();
+
+	    assertEquals(expected, actual);
+    }
+
 }
