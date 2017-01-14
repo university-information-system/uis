740513cc6ee8a9fcb0f1aa08944d1152c6163099
Martin Stücklschwaiger <martin.st@wolke7.net>
2017-01-15 00:00:07 +0100

    Add start dates to Semester Types

--- src/main/java/at/ac/tuwien/inso/entity/SemesterType.java
+++ src/main/java/at/ac/tuwien/inso/entity/SemesterType.java
@@ -1,14 +1,39 @@
 package at.ac.tuwien.inso.entity;
 
+
 public enum SemesterType {
 
-    WinterSemester("WS"),
-    SummerSemester("SS");
+    WinterSemester("WS", 10, 1),
+    SummerSemester("SS", 3, 1);
 
+    /**
+     * Name of the semester: WS or SS
+     */
     private final String name;
 
-    SemesterType(String name) {
+    /**
+     * Month the semester starts
+     */
+    private final int startMonth;
+
+    /**
+     * Day in month the semester starts
+     */
+    private final int startDay;
+
+
+    SemesterType(String name, int startMonth, int startDay) {
         this.name = name;
+        this.startMonth = startMonth;
+        this.startDay = startDay;
+    }
+
+    public int getStartMonth() {
+        return startMonth;
+    }
+
+    public int getStartDay() {
+        return startDay;
     }
 
     @Override
