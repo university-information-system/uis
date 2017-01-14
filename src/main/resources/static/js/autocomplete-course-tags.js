46a20e7b2ed3ab8d6e92e96299e098d1530f6372
Enri Miho <e0929003@student.tuwien.ac.at>
2017-01-14 22:11:35 +0100

    class needed for autocomplete chips

---
+++ src/main/resources/static/js/autocomplete-course-tags.js
@@ -0,0 +1,59 @@
+export default class AutoCompleteCourseTags {
+
+    initialize() {
+        $( function() {
+
+            if($('#tags').length) {
+
+                if($('#courseIdForTags').length) {
+                    $.ajax({
+                        url: "/lecturer/courses/json/tags",
+                        data: { courseId: $('#courseIdForTags').val() },
+                        type: 'GET',
+                        success: function (data) {
+                            for(var i=0; i< data.length; i++) {
+                                $('#tags').materialtags('add', data[i].name);
+                            }
+                            $('#tagsData').val($('#tags').materialtags()[0].itemsArray);
+                        }
+                    });
+                }
+            }
+
+            var allTags = new Bloodhound({
+                datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+                queryTokenizer: Bloodhound.tokenizers.whitespace,
+                prefetch: {
+                    url: '/lecturer/courses/json/tags',
+                    filter: function(list) {
+                        return $.map(list, function(name) {
+                            return { name: name };
+                        });
+                    }
+                }
+            });
+            allTags.initialize();
+            $('#tags').materialtags({
+                freeInput: true,
+                typeaheadjs: {
+                    name: 'allTags',
+                    displayKey: 'name',
+                    valueKey: 'name',
+                    source: allTags.ttAdapter()
+                }
+            });
+
+            $('#tags').on('itemAdded', function(event) {
+                $('#tagsData').val($('#tags').materialtags()[0].itemsArray);
+            });
+
+            $('#tags').on('itemRemoved', function(event) {
+                $('#tagsData').val($('#tags').materialtags()[0].itemsArray);
+            });
+
+        });
+
+    };
+
+
+}
