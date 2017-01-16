40f17af886ebf119d08156fd71fa65d9dc5bd703
Michael Pazourek <e1225721@student.tuwien.ac.at>
2017-01-16 14:34:58 +0100

    added  javadoc to tagservice

--- src/main/java/at/ac/tuwien/inso/service/TagService.java
+++ src/main/java/at/ac/tuwien/inso/service/TagService.java
@@ -8,9 +8,21 @@ import java.util.*;
 
 public interface TagService {
 
+	/**
+	 * returns all tags.
+	 * can only be used if a user is authenticated
+	 * 
+	 * @return a list of {@link Tag}s
+	 */
     @PreAuthorize("isAuthenticated()")
     List<Tag> findAll();
 
+    /**
+     * returns a tag by name
+     * can only be used if a user is authenticated
+     * @param name
+     * @return a {@link Tag} object
+     */
     @PreAuthorize("isAuthenticated()")
     Tag findByName(String name);
 }
