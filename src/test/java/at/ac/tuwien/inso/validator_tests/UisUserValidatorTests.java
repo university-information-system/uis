c1e6155602f7bc77b58fa6483a5e9d7a0271f4cd
Martin Stücklschwaiger <martin.st@wolke7.net>
2017-01-17 23:02:29 +0100

    Add tests for email validation

---
+++ src/test/java/at/ac/tuwien/inso/validator_tests/UisUserValidatorTests.java
@@ -0,0 +1,54 @@
+package at.ac.tuwien.inso.validator_tests;
+
+
+import org.junit.Test;
+
+import at.ac.tuwien.inso.exception.ValidationException;
+import at.ac.tuwien.inso.service.validator.UisUserValidator;
+
+
+public class UisUserValidatorTests {
+
+    private final UisUserValidator validator = new UisUserValidator();
+
+    @Test
+    public void testEmailValidator() {
+        validator.validateEmail("uis@tuwien.ac.at");
+    }
+
+    @Test (expected = ValidationException.class)
+    public void testEmailInvalid() {
+        validator.validateEmail("uis");
+    }
+
+    /**
+     * Local email (for intranets) should not be allowed
+     */
+    @Test (expected = ValidationException.class)
+    public void testEmailLocal() {
+        validator.validateEmail("uis@tuwien");
+    }
+
+    @Test (expected = ValidationException.class)
+    public void testEmailNoLocalPart() {
+        validator.validateEmail("@tuwien.at");
+    }
+
+    @Test (expected = ValidationException.class)
+    public void testEmailValidatorNull() {
+        validator.validateEmail(null);
+    }
+
+    @Test (expected = ValidationException.class)
+    public void testEmailValidatorEmpty() {
+        validator.validateEmail("");
+    }
+
+    /**
+     * German umlauts are valid in emails
+     */
+    @Test
+    public void testEmailValidatorUmlauts() {
+        validator.validateEmail("björn@tüdlü.at");
+    }
+}
