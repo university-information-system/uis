446b9de70eb1717e2de815ae08a8db6758499bb1
Vlad - Ioan Balan <balan_vlad92@yahoo.com>
2017-01-21 11:49:33 +0100

    Fixes MandatoryCourseScorer

--- src/main/java/at/ac/tuwien/inso/service/course_recommendation/impl/MandatoryCourseScorer.java
+++ src/main/java/at/ac/tuwien/inso/service/course_recommendation/impl/MandatoryCourseScorer.java
@@ -2,13 +2,17 @@ package at.ac.tuwien.inso.service.course_recommendation.impl;
 
 import at.ac.tuwien.inso.entity.Course;
 import at.ac.tuwien.inso.entity.Student;
+import at.ac.tuwien.inso.entity.Subject;
+import at.ac.tuwien.inso.repository.SubjectForStudyPlanRepository;
 import at.ac.tuwien.inso.service.course_recommendation.CourseScorer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
 
 import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
 
 @Service
@@ -16,20 +20,25 @@ public class MandatoryCourseScorer implements CourseScorer {
 
     private static final double MANDATORY = 3;
 
+    @Autowired
+    private SubjectForStudyPlanRepository subjectForStudyPlanRepository;
+
     @Override
     public Map<Course, Double> score(List<Course> courses, Student student) {
         Map<Course, Double> scoredCourses = courses.stream().collect(toMap(identity(), it -> 0.0));
+        List<Subject> subjectsForCourses = courses.stream().map(course -> course.getSubject()).collect(toList());
 
-        courses.forEach(course ->
-                student.getStudyplans().forEach(studyplan ->
-                        studyplan.getStudyplan().getSubjects().stream()
-                                .filter(subjectForStudyPlan ->
-                                        subjectForStudyPlan.getSubject().equals(course.getSubject()))
-                                .filter(subjectForStudyPlan ->
-                                        subjectForStudyPlan.getMandatory())
-                                .forEach(subjectForStudyPlan ->
-                                        scoredCourses.put(course, scoredCourses.get(course) + MANDATORY))
-                )
+        student.getStudyplans().forEach(studyPlanRegistration ->
+                subjectForStudyPlanRepository.findBySubjectInAndStudyPlan(
+                        subjectsForCourses,
+                        studyPlanRegistration.getStudyplan()
+                ).stream()
+                        .filter(subjectForStudyPlan -> subjectForStudyPlan.getMandatory())
+                        .forEach(subjectForStudyPlan -> {
+                            Course course = courses.stream().filter(it -> it.getSubject().equals(subjectForStudyPlan.getSubject())).findFirst().get();
+                            scoredCourses.put(course, scoredCourses.get(course) + MANDATORY);
+                        })
+
         );
 
         return scoredCourses;
