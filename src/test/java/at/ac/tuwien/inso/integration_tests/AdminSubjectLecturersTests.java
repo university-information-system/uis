08e14099f30414c960cef548ba2eef353e911b30
Enri Miho <e0929003@student.tuwien.ac.at>
2017-01-22 01:00:26 +0100

    add TODOs

--- src/test/java/at/ac/tuwien/inso/integration_tests/AdminSubjectLecturersTests.java
+++ src/test/java/at/ac/tuwien/inso/integration_tests/AdminSubjectLecturersTests.java
@@ -1,7 +1,32 @@
 package at.ac.tuwien.inso.integration_tests;
 
-/**
- * Created by enri on 21.01.17.
- */
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
 public class AdminSubjectLecturersTests {
+
+    @Test
+    public void addLecturerToSubjectTest() {
+        //TODO
+    }
+
+    @Test
+    public void removeLecturerFromSubjectTest() {
+        //TODO
+    }
+
+    @Test
+    public void availableLecturersJsonTest() {
+        //TODO
+    }
 }
