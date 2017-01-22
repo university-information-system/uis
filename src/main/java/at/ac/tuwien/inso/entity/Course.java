1583c8b36c239d28be872002e1d1b38c93fea712
Michael Pazourek <e1225721@student.tuwien.ac.at>
2017-01-23 00:03:47 +0100

    fixed semester dto todo

--- src/main/java/at/ac/tuwien/inso/entity/Course.java
+++ src/main/java/at/ac/tuwien/inso/entity/Course.java
@@ -14,6 +14,8 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
+import at.ac.tuwien.inso.dto.SemesterDto;
+
 @Entity
 public class Course {
 
@@ -42,11 +44,12 @@ public class Course {
     protected Course() {
     }
 
-    /*
-     * TODO use DTO here
-     */
     public Course(Subject subject, Semester semester) {
         this(subject, semester, "");
+    }
+    
+    public Course(Subject subject, SemesterDto semester) {
+        this(subject, semester.toEntity(), "");
     }
 
     public Course(Subject subject, Semester semester, String description) {
