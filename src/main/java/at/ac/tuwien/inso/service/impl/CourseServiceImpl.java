e4734de3d25bb88124699941ff6b5c1c5781d31a
Damjan Gjurovski <e1129609@student.tuwien.ac.at>
2017-01-23 00:51:11 +0100

    Add find courses of a certain subject in the current semester

--- src/main/java/at/ac/tuwien/inso/service/impl/CourseServiceImpl.java
+++ src/main/java/at/ac/tuwien/inso/service/impl/CourseServiceImpl.java
@@ -59,7 +59,7 @@ public class CourseServiceImpl implements CourseService {
 
     @Autowired
     private UserAccountService userAccountService;
-    
+
     @Autowired
     private GradeService gradeService;
 
@@ -75,7 +75,7 @@ public class CourseServiceImpl implements CourseService {
     @Override
     @Transactional(readOnly = true)
     public Page<Course> findCourseForCurrentSemesterWithName(@NotNull String name, Pageable pageable) {
-    	log.info("try to find course for current semester with semestername: "+name+"and pageable "+pageable);
+        log.info("try to find course for current semester with semestername: " + name + "and pageable " + pageable);
         Semester semester = semesterService.getOrCreateCurrentSemester().toEntity();
         return courseRepository.findAllBySemesterAndSubjectNameLikeIgnoreCase(semester, "%" + name + "%", pageable);
     }
@@ -83,41 +83,46 @@ public class CourseServiceImpl implements CourseService {
     @Override
     @Transactional(readOnly = true)
     public List<Course> findCoursesForCurrentSemesterForLecturer(Lecturer lecturer) {
-    	log.info("try finding courses for current semester for lecturer with id "+lecturer.getId());
+        log.info("try finding courses for current semester for lecturer with id " + lecturer.getId());
         Semester semester = semesterService.getOrCreateCurrentSemester().toEntity();
         Iterable<Subject> subjectsForLecturer = subjectRepository.findByLecturers_Id(lecturer.getId());
         List<Course> courses = new ArrayList<>();
         subjectsForLecturer.forEach(subject -> courses.addAll(courseRepository.findAllBySemesterAndSubject(semester, subject)));
         return courses;
     }
-    
+
     @Override
     @Transactional(readOnly = true)
     public List<Course> findCoursesForSubject(Subject subject) {
-    	log.info("try finding course for subject with id "+subject.getId());
-    	return courseRepository.findAllBySubject(subject);    	
-    	
+        log.info("try finding course for subject with id " + subject.getId());
+        return courseRepository.findAllBySubject(subject);
+    }
+
+    @Override
+    public List<Course> findCoursesForSubjectAndCurrentSemester(Subject subject) {
+        List<Course> result = courseRepository.findAllBySemesterAndSubject(semesterService.getCurrentSemester().toEntity(), subject);
+        return result;
     }
 
     @Override
     @Transactional
     public Course saveCourse(AddCourseForm form) {
-    	log.info("try saving course");
+        log.info("try saving course");
         Course course = form.getCourse();
         validator.validateNewCourse(course);
-        log.info("try saving course "+course.toString());
+        log.info("try saving course " + course.toString());
 
         ArrayList<Tag> currentTagsOfCourse = new ArrayList<>(form.getCourse().getTags());
 
-        for(String tag : form.getTags()) {
+        for (String tag : form.getTags()) {
             Tag newTag = tagService.findByName(tag);
 
             // tag doesn't exist, so create a new one.
-            if(newTag == null) {
+            if (newTag == null) {
                 course.addTags(new Tag(tag));
             }
             // tag exists, but not in this course
-            else if(!course.getTags().contains(newTag)) {
+            else if (!course.getTags().contains(newTag)) {
                 course.addTags(newTag);
             }
             // tag already exists for this course
@@ -138,46 +143,46 @@ public class CourseServiceImpl implements CourseService {
     @Override
     @Transactional(readOnly = true)
     public Course findOne(Long id) {
-    	log.info("try finding course with id "+id);
+        log.info("try finding course with id " + id);
         Course course = courseRepository.findOne(id);
         if (course == null) {
-        	log.warn("Course with id " + id + " does not exist");
+            log.warn("Course with id " + id + " does not exist");
             throw new BusinessObjectNotFoundException("Course with id " + id + " does not exist");
         }
         return course;
     }
-    
+
     @Override
     @Transactional
-    public boolean remove(Long courseId) throws ValidationException{
-    	log.info("try removing  course with id "+courseId);
+    public boolean remove(Long courseId) throws ValidationException {
+        log.info("try removing  course with id " + courseId);
         validator.validateCourseId(courseId); // throws ValidationException
-    	Course course = courseRepository.findOne(courseId);
-    	
-    	if(course==null){
-    		String msg = "Course can not be deleted because there is no couse found with id "+courseId;
-    		log.warn(msg);
-    		throw new BusinessObjectNotFoundException(msg);
-    	}
-    	
+        Course course = courseRepository.findOne(courseId);
 
-    	List<Grade> grades = gradeService.findAllByCourseId(courseId);
-    	
-    	if(grades!=null&&!grades.isEmpty()){
-    		String msg = "There are grades for course [id:"+courseId+"], therefore it can not be removed.";
-    		log.warn(msg);
-    		throw new ValidationException(msg);
-    	}
-        
-    	if(!course.getStudents().isEmpty()){
-    		String msg = "There are students for course [id:"+courseId+"], therefore it can not be removed.";
-    		log.warn(msg);
-    		throw new ValidationException(msg);
-    	}    	
-        
-    	log.info("successfully validated course removal. removing now!");
-    	courseRepository.delete(course);
-    	return true;
+        if (course == null) {
+            String msg = "Course can not be deleted because there is no couse found with id " + courseId;
+            log.warn(msg);
+            throw new BusinessObjectNotFoundException(msg);
+        }
+
+
+        List<Grade> grades = gradeService.findAllByCourseId(courseId);
+
+        if (grades != null && !grades.isEmpty()) {
+            String msg = "There are grades for course [id:" + courseId + "], therefore it can not be removed.";
+            log.warn(msg);
+            throw new ValidationException(msg);
+        }
+
+        if (!course.getStudents().isEmpty()) {
+            String msg = "There are students for course [id:" + courseId + "], therefore it can not be removed.";
+            log.warn(msg);
+            throw new ValidationException(msg);
+        }
+
+        log.info("successfully validated course removal. removing now!");
+        courseRepository.delete(course);
+        return true;
     }
 
 
@@ -188,7 +193,7 @@ public class CourseServiceImpl implements CourseService {
         validator.validateCourseId(course.getId());
         Student student = studentRepository.findByUsername(userAccountService.getCurrentLoggedInUser().getUsername());
 
-		log.info("try registering currently logged in student with id "+student.getId()+" for course with id "+course.getId());
+        log.info("try registering currently logged in student with id " + student.getId() + " for course with id " + course.getId());
         if (course.getStudentLimits() <= course.getStudents().size()) {
             return false;
         } else if (course.getStudents().contains(student)) {
@@ -204,8 +209,8 @@ public class CourseServiceImpl implements CourseService {
     @Override
     @Transactional(readOnly = true)
     public List<Course> findAllForStudent(Student student) {
-        log.info("finding all courses for student with id "+student.getId());
-    	return courseRepository.findAllForStudent(student);
+        log.info("finding all courses for student with id " + student.getId());
+        return courseRepository.findAllForStudent(student);
     }
 
     @Override
@@ -229,7 +234,7 @@ public class CourseServiceImpl implements CourseService {
     public CourseDetailsForStudent courseDetailsFor(Student student, Long courseId) {
         validator.validateCourseId(courseId);
         validator.validateStudent(student);
-        log.info("reading course details for student with id "+student.getId()+" from course with id "+ courseId);
+        log.info("reading course details for student with id " + student.getId() + " from course with id " + courseId);
         Course course = findOne(courseId);
         if (course == null) {
             log.warn("Course with id {} not found. Nothing to unregister", courseId);
@@ -237,9 +242,7 @@ public class CourseServiceImpl implements CourseService {
         }
 
 
-        return new CourseDetailsForStudent(course)
-                .setCanEnroll(canEnrollToCourse(student, course))
-                .setStudyplans(subjectForStudyPlanRepository.findBySubject(course.getSubject()));
+        return new CourseDetailsForStudent(course).setCanEnroll(canEnrollToCourse(student, course)).setStudyplans(subjectForStudyPlanRepository.findBySubject(course.getSubject()));
     }
 
     @Override
@@ -251,7 +254,6 @@ public class CourseServiceImpl implements CourseService {
     private boolean canEnrollToCourse(Student student, Course course) {
         validator.validateCourse(course);
         validator.validateStudent(student);
-        return course.getSemester().toDto().equals(semesterService.getOrCreateCurrentSemester()) &&
-                !courseRepository.existsCourseRegistration(student, course);
+        return course.getSemester().toDto().equals(semesterService.getOrCreateCurrentSemester()) && !courseRepository.existsCourseRegistration(student, course);
     }
 }
