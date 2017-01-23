bbb50d0485d6196945420cede3a78afce7c20fa4
Enri Miho <e0929003@student.tuwien.ac.at>
2017-01-23 15:06:06 +0100

    test removing a lecturer from a subject

--- src/test/java/at/ac/tuwien/inso/integration_tests/AdminSubjectLecturersTests.java
+++ src/test/java/at/ac/tuwien/inso/integration_tests/AdminSubjectLecturersTests.java
@@ -2,9 +2,9 @@ package at.ac.tuwien.inso.integration_tests;
 
 import at.ac.tuwien.inso.entity.Lecturer;
 import at.ac.tuwien.inso.entity.Subject;
-import at.ac.tuwien.inso.entity.SubjectForStudyPlan;
 import at.ac.tuwien.inso.repository.SubjectRepository;
 import at.ac.tuwien.inso.repository.UisUserRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +20,14 @@ import java.math.BigDecimal;
 
 import static java.util.Arrays.asList;
 import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -41,27 +46,47 @@ public class AdminSubjectLecturersTests {
     @Autowired
     private SubjectRepository subjectRepository;
 
+    private Lecturer lecturer1 = new Lecturer("l1", "lecturer1", "l1@uis.at");
+    private Lecturer lecturer2 = new Lecturer("l2", "lecturer2", "l2@uis.at");
+    private Lecturer lecturer3 = new Lecturer("l3", "lecturer3", "l3@uis.at");
+    private Subject ase = new Subject("ase", new BigDecimal(3.0));
+
+    @Before
+    public void setUp() {
+        uisUserRepository.save(asList(lecturer1, lecturer2, lecturer3));
+        ase.addLecturers(lecturer1);
+        subjectRepository.save(ase);
+    }
+
     @Test
     public void addLecturerToSubjectTest() {
         //TODO
     }
 
     @Test
-    public void removeLecturerFromSubjectTest() {
-        //TODO
+    public void removeLecturerFromSubjectTest() throws Exception {
+
+        // given 3 lecturers, where lecturer1 is a already a lecturer of the subject ase
+
+        // when removing lecturer1 from subject ase
+        mockMvc.perform(
+                post("/admin/subjects/" + ase.getId() + "/lecturers/"+ lecturer1.getId() +"/delete")
+                        .param("subjectId", ase.getId().toString())
+                        .param("lecturerId", lecturer1.getId().toString())
+                        .with(user("admin").roles("ADMIN"))
+                        .with(csrf())
+        ).
+        andExpect(
+                redirectedUrl("/admin/subjects/" + ase.getId().toString())
+        );
+
+        assertEquals(asList(), ase.getLecturers());
     }
 
     @Test
     public void availableLecturersJsonTest() throws Exception {
 
         // given 3 lecturers, where lecturer1 is a already a lecturer of the subject ase
-        Lecturer lecturer1 = new Lecturer("l1", "lecturer1", "l1@uis.at");
-        Lecturer lecturer2 = new Lecturer("l2", "lecturer2", "l2@uis.at");
-        Lecturer lecturer3 = new Lecturer("l3", "lecturer3", "l3@uis.at");
-        uisUserRepository.save(asList(lecturer1, lecturer2, lecturer3));
-        Subject ase = new Subject("ase", new BigDecimal(3.0));
-        ase.addLecturers(lecturer1);
-        subjectRepository.save(ase);
 
         // when searching for "lecturer"
         MvcResult result =  mockMvc.perform(
