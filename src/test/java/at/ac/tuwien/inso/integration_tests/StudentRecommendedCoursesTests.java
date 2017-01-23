11631e9c09d5d7e7a4f8b4950f649a692f5b0fe1
Enri Miho <e0929003@student.tuwien.ac.at>
2017-01-23 14:20:36 +0100

    add test class for recommended courses.

---
+++ src/test/java/at/ac/tuwien/inso/integration_tests/StudentRecommendedCoursesTests.java
@@ -0,0 +1,22 @@
+package at.ac.tuwien.inso.integration_tests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
+public class StudentRecommendedCoursesTests {
+
+    @Test
+    public void studentRecommendedCoursesTest() {
+        //TODO
+    }
+}
