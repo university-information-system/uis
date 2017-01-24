a317271c7f6660f2e3ab4def6019a6c6d791af2e
Damjan Gjurovski <e1129609@student.tuwien.ac.at>
2017-01-24 01:47:19 +0100

    Add filter to the recommendation algorithm to excludes dismissed courses

---
+++ src/main/java/at/ac/tuwien/inso/service/course_recommendation/filters/DismissedCoursesFilter.java
@@ -0,0 +1,18 @@
+package at.ac.tuwien.inso.service.course_recommendation.filters;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import at.ac.tuwien.inso.entity.Course;
+import at.ac.tuwien.inso.entity.Student;
+
+@Component
+public class DismissedCoursesFilter implements CourseRelevanceFilter {
+
+    @Override
+    public List<Course> filter(List<Course> courses, Student student) {
+        return courses.stream().filter(course -> !student.getDismissedCourses().contains(course)).collect(Collectors.toList());
+    }
+}
