5409d7bbc3572cbf9ff0c6965e8fa032ad5c0e77
Martin Stücklschwaiger <martin.st@wolke7.net>
2017-01-28 17:52:50 +0100

    Fix code style

--- src/main/java/at/ac/tuwien/inso/service/impl/SemesterServiceImpl.java
+++ src/main/java/at/ac/tuwien/inso/service/impl/SemesterServiceImpl.java
@@ -24,8 +24,8 @@ import at.ac.tuwien.inso.service.SemesterService;
 
 @Service
 public class SemesterServiceImpl implements SemesterService {
-	
-	private static final Logger log = LoggerFactory.getLogger(SemesterServiceImpl.class);
+
+    private static final Logger log = LoggerFactory.getLogger(SemesterServiceImpl.class);
 
     @Autowired
     private SemesterRepository semesterRepository;
@@ -36,21 +36,21 @@ public class SemesterServiceImpl implements SemesterService {
     @Override
     @Transactional
     public SemesterDto create(SemesterDto semester) {
-    	log.info("creating semester "+semester.getLabel());
+        log.info("creating semester " + semester.getLabel());
         return semesterRepository.save(semester.toEntity()).toDto();
     }
 
     @Override
     @Transactional(readOnly = true)
     public SemesterDto getCurrentSemester() {
-        Semester s = semesterRepository.findFirstByOrderByIdDesc();
-        
-        if(s!=null){
-        	log.info("returning current semester "+s.getLabel());
-        	return s.toDto();
-        }else{
-        	log.warn("current semester is null");
-        	return null;
+        Semester semester = semesterRepository.findFirstByOrderByIdDesc();
+
+        if (semester != null){
+            log.info("returning current semester " + semester.getLabel());
+            return semester.toDto();
+        } else {
+            log.warn("current semester is null");
+            return null;
         }
     }
 
@@ -82,21 +82,21 @@ public class SemesterServiceImpl implements SemesterService {
     @Override
     @Transactional(readOnly = true)
     public List<SemesterDto> findAll() {
-    	log.info("finding all semesters");
+        log.info("finding all semesters");
         return convertSemesterListToSemesterDtoList(semesterRepository.findAllByOrderByIdDesc());
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<SemesterDto> findAllSince(SemesterDto semester) {
-    	log.info("finding all semesters since "+semester.getLabel());
+        log.info("finding all semesters since " + semester.getLabel());
         return convertSemesterListToSemesterDtoList(semesterRepository.findAllSince(semester.toEntity()));
     }
     
     private List<SemesterDto> convertSemesterListToSemesterDtoList(List<Semester> semesters){
-    	List<SemesterDto> toReturn = new ArrayList<SemesterDto>();
+        List<SemesterDto> toReturn = new ArrayList<>();
         for(Semester s : semesters){
-        	toReturn.add(s.toDto());
+            toReturn.add(s.toDto());
         }
         return toReturn;
     }
