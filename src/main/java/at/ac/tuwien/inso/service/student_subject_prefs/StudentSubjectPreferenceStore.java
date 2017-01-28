5543e2969c192fef55d455e82f9294fcabdc0619
Michael Pazourek <e1225721@student.tuwien.ac.at>
2017-01-28 16:00:46 +0100

    added javadoc

--- src/main/java/at/ac/tuwien/inso/service/student_subject_prefs/StudentSubjectPreferenceStore.java
+++ src/main/java/at/ac/tuwien/inso/service/student_subject_prefs/StudentSubjectPreferenceStore.java
@@ -8,13 +8,19 @@ public interface StudentSubjectPreferenceStore {
 
 	/**
 	 * adds the student and the course to the StudentSubjectPreferenceRepository
-	 * means that the student has a preference for that course because he registered it
+	 * means that the student has a preference for the subject of the course because he registered it
 	 * 
 	 * @param student should not be null and id should not be null
 	 * @param course should not be null, should have an subject that is not null and that subject should have an id that is not null
 	 */
     void studentRegisteredCourse(Student student, Course course);
 
+    /**
+     * removes the preference of a student for a subject (the subject for the given course) from the StudentSubjectPreferenceRepository
+     * 
+     * @param student should not be null and id should not be null
+	 * @param course should not be null, should have an subject that is not null and that subject should have an id that is not null
+     */
     void studentUnregisteredCourse(Student student, Course course);
 
     void studentGaveCourseFeedback(Student student, Feedback feedback);
