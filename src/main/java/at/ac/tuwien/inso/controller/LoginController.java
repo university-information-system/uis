372f1321d4c00b67ec327ec945d36e9007ddae0b
Martin Stücklschwaiger <martin.st@wolke7.net>
2017-01-28 15:50:57 +0100

    Redirect already logged in users

--- src/main/java/at/ac/tuwien/inso/controller/LoginController.java
+++ src/main/java/at/ac/tuwien/inso/controller/LoginController.java
@@ -1,28 +1,32 @@
 package at.ac.tuwien.inso.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.Map;
-
 import javax.servlet.http.HttpSession;
 
+import at.ac.tuwien.inso.entity.UserAccount;
+
 @Controller
-@RequestMapping("/login")
 public class LoginController {
 
-	@GetMapping
+    private static final Logger log = LoggerFactory.getLogger(LoginController.class);
+
+	@GetMapping("/login")
 	public String getLogin(
-			@RequestParam(value = "loggedOut", required = false) String loggedOut,
-			@RequestParam(value = "error", required = false) String error,
+            @RequestParam(value = "loggedOut", required = false) String loggedOut,
+            @RequestParam(value = "error", required = false) String error,
+            @RequestParam(value = "invalidSession", required = false) String invalidSession,
 			HttpSession session,
-            Model model,
 			RedirectAttributes redirectAttributes
 	) {
+
 	    // Show a flash messages after a logout
 		if (loggedOut != null) {
 			redirectAttributes.addFlashAttribute("flashMessage", "login.loggedOut");
@@ -37,9 +41,32 @@ public class LoginController {
                 redirectAttributes.addFlashAttribute("loginError", lastException.getLocalizedMessage());
             }
 
+            log.info("Removing ?error parameter from /login");
 			return "redirect:/login";
 		}
 
-		return "login";
+		// Show a message after the session became invalid
+        if (invalidSession != null) {
+            redirectAttributes.addFlashAttribute("flashMessage", "login.invalidSession");
+            log.info("Removing ?invalidSession parameter from /login");
+            return "redirect:/login";
+        }
+
+        // Redirect to "/" if the user is already logged in
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Object userAccount = auth.getPrincipal();
+
+        if (userAccount != null && userAccount instanceof UserAccount) {
+            UserAccount castUserAccount = (UserAccount) userAccount;
+            Long userId = castUserAccount.getId();
+
+            if (userId != null && userId > 0) {
+                log.info("User " + userId + " is already logged in");
+                redirectAttributes.addFlashAttribute("flashMessage", "login.already-logged-in");
+                return "redirect:/";
+            }
+        }
+
+        return "login";
 	}
 }
