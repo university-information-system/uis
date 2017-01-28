ab5b1618d63ad68b554f89dda038eb0fc69a45bb
Valentin - Mihai Neacsu <neacsustoi_92@yahoo.com>
2017-01-28 19:14:53 +0100

    add comment for student neighborhood store

--- src/main/java/at/ac/tuwien/inso/service/course_recommendation/user_based/StudentNeighborhoodStore.java
+++ src/main/java/at/ac/tuwien/inso/service/course_recommendation/user_based/StudentNeighborhoodStore.java
@@ -1,8 +1,19 @@
 package at.ac.tuwien.inso.service.course_recommendation.user_based;
 
-import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.neighborhood.*;
 
 public interface StudentNeighborhoodStore {
 
+    /**
+     * Computes and returns the neighbourhood based on the similarity
+     * index for students.
+     * <p>
+     * Implementors of this may also cache the similarity index to avoid
+     * its expensive computation. Therefore, it is not guaranteed that this
+     * method returns an up-to-date view of the user similarities, as reflected
+     * by the most recent student activities.
+     *
+     * @return the similarity neighborhood for students
+     */
     UserNeighborhood getStudentNeighborhood();
 }
